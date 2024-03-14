efc7a90a65db49f460e688ef1183b4e957ccbaae
Rupesh Thakur <139684026+rupeshthakur8550@users.noreply.github.com>
2024-03-14 20:43:22 +0530

    Time: 74 ms (93.44%), Space: 59.4 MB (49.72%) - LeetHub

--- 2618-check-if-object-instance-of-class/2618-check-if-object-instance-of-class.js
+++ 2618-check-if-object-instance-of-class/2618-check-if-object-instance-of-class.js
@@ -5,9 +5,18 @@
  */
 
 var checkIfInstanceOf = function(obj, classFunction) {
-    if(obj === null || obj === undefined || typeof classFunction !== 'function')
+    if(obj === null || obj === undefined || typeof classFunction !== 'function') {
         return false;
-    return Object(obj) instanceof classFunction;
+    } else {
+        let currProto = Object.getPrototypeOf(obj)
+        while (currProto !== null) {
+            if(currProto === classFunction.prototype) {
+                return true;
+            }
+            currProto = Object.getPrototypeOf(currProto);
+        }
+        return false;
+    }
 };
 
 /**
