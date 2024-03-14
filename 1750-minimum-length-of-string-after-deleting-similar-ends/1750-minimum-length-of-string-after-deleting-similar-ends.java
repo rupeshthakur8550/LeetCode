4bb8136961fb9a1d466a1198bb108a5e71bc13ba
Rupesh Thakur <139684026+rupeshthakur8550@users.noreply.github.com>
2024-03-14 20:55:50 +0530

    Time: 4 ms (76.92%), Space: 45 MB (81.64%) - LeetHub

---
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minimumLength(String s) {
+        if (s.isEmpty()) {
+            return 0;
+        }
+        return calculateLength(s);
+    }
+
+    private int calculateLength(String check) {
+        int start = 0, end = check.length() - 1;
+        while (start < end) {
+			if(check.charAt(start) == check.charAt(end)) {
+				char ch = check.charAt(start);
+				while(start <= end && check.charAt(start) == ch) {
+					start++;
+				}
+				while(start <= end && check.charAt(end) == ch) {
+					end--;
+				}
+			}
+			else {
+				break;
+			}
+		}
+		return end < start ? 0 : end - start + 1;
+    }
+}
