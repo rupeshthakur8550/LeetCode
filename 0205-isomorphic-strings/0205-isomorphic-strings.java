6d874875f8edd2162a35bc3a0e1ea528a3fba718
Rupesh Thakur <139684026+rupeshthakur8550@users.noreply.github.com>
2024-04-02 11:24:06 +0530

    Time: 20 ms (15.77%), Space: 43.4 MB (9.27%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -4,6 +4,7 @@ class Solution {
         for (Integer i=0; i<s.length(); ++i)
             if (map.put(s.charAt(i), i) != map.put(t.charAt(i)+"", i))
                 return false;
+        System.gc();
         return true;
     }
 }
