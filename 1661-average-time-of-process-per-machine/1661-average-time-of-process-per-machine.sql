e63d7ba00de640db0dd84cc2ad415b369efa22be
Rupesh Thakur <139684026+rupeshthakur8550@users.noreply.github.com>
2024-07-17 13:07:18 +0530

    Time: 198 ms (86.50%), Space: 0B (100.00%) - LeetHub

--- 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -1,6 +1,11 @@
 # Write your MySQL query statement below
-SELECT 
-    machine_id,
-    ROUND(AVG(IF(activity_type = "end", timestamp, -timestamp)) * 2, 3) AS processing_time
-FROM Activity
-GROUP BY machine_id;
+SELECT machine_id,ROUND(
+    AVG(
+        CASE 
+            WHEN activity_type = 'start' THEN -timestamp
+            ELSE timestamp
+        END
+    ) * 2, -- 2 is the partition as which start and end makes 1 partition here we have 2 partition for each
+    3 
+) AS processing_time
+FROM Activity GROUP BY machine_id;
