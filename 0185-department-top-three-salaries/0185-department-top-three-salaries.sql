cae853297df5fb329c2b9238f538d854bc6568e9
Rupesh Thakur <139684026+rupeshthakur8550@users.noreply.github.com>
2024-07-19 13:32:46 +0530

    Time: 818 ms (76.12%), Space: 0B (100.00%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,16 +1,26 @@
 # Write your MySQL query statement below
-WITH employee_department AS 
-(
-    SELECT 
-        d.name AS Department, 
-        e.salary AS Salary, 
-        e.name AS Employee, 
-        DENSE_RANK() OVER(PARTITION BY d.name ORDER BY e.salary DESC) AS dept_rank
-    FROM Employee e 
-    JOIN Department d
-    ON e.departmentId = d.id
-)
+# WITH employee_department AS 
+# (
+#     SELECT 
+#         d.name AS Department, 
+#         e.salary AS Salary, 
+#         e.name AS Employee, 
+#         DENSE_RANK() OVER(PARTITION BY d.name ORDER BY e.salary DESC) AS dept_rank
+#     FROM Employee e 
+#     JOIN Department d
+#     ON e.departmentId = d.id
+# )
 
-SELECT Department, Employee, Salary 
-FROM employee_department
-WHERE dept_rank <= 3;
+# SELECT Department, Employee, Salary 
+# FROM employee_department
+# WHERE dept_rank <= 3;
+
+SELECT Department, Employee, Salary FROM
+(
+    SELECT d.name as Department,
+           e.name as Employee,
+           e.salary as Salary,
+    DENSE_RANK() OVER(PARTITION BY d.name ORDER BY e.salary DESC) AS salary_rank
+    FROM Employee e JOIN Department d 
+    ON e.departmentId = d.id
+) combined_data WHERE salary_rank <=3;
