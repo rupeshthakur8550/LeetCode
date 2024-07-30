f9acaec5cc22f7da24d80251926a7ee09343be36
Rupesh Thakur <139684026+rupeshthakur8550@users.noreply.github.com>
2024-07-30 16:10:02 +0530

    Time: 21 ms (84.96%), Space: 45.5 MB (23.12%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
@@ -1,24 +1,19 @@
 class Solution {
     public int minimumDeletions(String s) {
-        int l = s.length();
-		//dp stores number of chars to remove to make s.substring(0, i) valid
-        int[] dp = new int[l + 1];
-        int bcount = 0;
-        for (int i = 0; i < l; i++) {
-            if (s.charAt(i) == 'a') {
-                //case 1: keep current a. ==> prev chars must be a...a
-                //so need to remove all 'b' chars before i, which is bcount
-                
-                //case 2: remove current a ==> prev chars must be a...ab...b
-                //so need to remove current a and whatever makes substring before current i valid which is dp[i];
-                dp[i + 1] = Math.min(dp[i] + 1, bcount);
+        int n = s.length();
+        int minDeletions = 0;
+        int bCount = 0;
+
+        // minDeletions variable represents dp[i]
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == 'b') {
+                bCount++;
             } else {
-                //since it is always valid to append 'b' if substring before current i is valid, so just copy whatever makes substring before i valid which is dp[i];
-                dp[i + 1] = dp[i];
-                bcount++;
+                // Two cases: remove 'a' or keep 'a'
+                minDeletions = Math.min(minDeletions + 1, bCount);
             }
         }
-        
-        return dp[l];
+
+        return minDeletions;
     }
 }
